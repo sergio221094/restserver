6b7b4fe2ad730dc558fcc927918c39e43c8ab8bb
Sergio <sergio221094@gmail.com>
2019-03-05 16:02:13 -0500

    Métodos HTTP conectados a base de datos MONGO

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,36 +1,106 @@
 const express = require('express');
 const app = express();
+const _ = require('underscore');
+const Usuario = require('../models/usuario')
+
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
+    Usuario.find({})
+        .exec((err, usuarios) => {
+            if (err) {
+                res.status(400).json({
+                    mensaje: 'Ocurrió un error al solicitar el usuario',
+                    err: err
+                })
+            } else {
+                res.json({
+                    ok: true,
+                    usuarios
+                });
+            }
+        });
+});
 
 app.post('/usuario', function(req, res) {
 
     let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            body
-        })
-    }
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        role: body.role
 
-})
+    });
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                mensaje: 'Ocurrió un error guardando al usuario, verifique los datos.'
+            })
+        } else {
+            res.json({
+                usuarioDB,
+                mensaje: 'Usuario guardado correctamente.'
+            });
+        }
     })
 })
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+    let cambiaEstado = {
+        estado: false
+    };
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado,
+            message: 'Usuario eliminado'
+        });
+
+    });
 });
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,6 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(require('./routes/routes'));
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando el puerto:", process.env.PORT);
