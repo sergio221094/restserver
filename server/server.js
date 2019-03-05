70312e76c08af12493ab9c1827eced94f9c98c46
Sergio <sergio221094@gmail.com>
2019-03-05 12:07:54 -0500

    Ajustes en el archivo server.js

--- server/server.js
+++ server/server.js
@@ -9,37 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
+app.use(require('./routes/routes'));
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            body
-        })
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
 app.listen(process.env.PORT, () => {
     console.log("Escuchando el puerto:", process.env.PORT);
 });
